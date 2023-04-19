f05363830adf3efe2c5a8b6dd03890dbe0c95d8e
maibelline <mayb80@hotmail.fr>
2023-04-19 19:49:41 +0200

    test update deploy

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -39,7 +39,7 @@ const Work = () => {
 
     return (
         <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-            <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Work</h1>
+            <h1 className='text-4xl font-bold text-center text-[#001b5e] m-5'>Work</h1>
             {data.map((item, idx)=>(
                 <WorkItem 
                 key={idx} 
